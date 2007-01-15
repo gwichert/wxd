ec505e51199f5a1a537a217eebf412ae4aff3e57
Anders F Bjorklund <afb@users.sourceforge.net>
2007-01-15 11:50:21 +0000

    renamed

--- wxc/wx_platform.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if [ "x$WX_CONFIG" = "x" ]; then WX_CONFIG=wx-config; fi
-# Outputs the current wxWidgets platform (from wx-config)
-$WX_CONFIG --cflags $@ | sed -e 's/.*\-D__\(WX.*\)__.*/\1/'
