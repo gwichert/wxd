bd4a215602a2ee10b2401579106197c374c87e1b
Anders F Bjorklund <afb@users.sourceforge.net>
2007-11-27 08:08:10 +0000

    fix workaround version code

--- wx/common.d
+++ wx/common.d
@@ -73,14 +73,14 @@ else //version(ANSI)
 	alias ubyte wxChar;
 
 //! \cond D1
-static if (version_major <= 1 && version_minor <= 16)
+static if (version_major < 1 || (version_major == 1 && version_minor < 16))
 //! \endcond
-	alias char[] string;
+	alias char[] string; // added in DMD 1.016 and DMD 2.000
 
 //! \cond D2
-static if (version_major < 2)
+static if (version_major < 2 || (version_major == 2 && version_minor < 6))
 //! \endcond
-	string assumeUnique(char[] s) { return s; }
+	string assumeUnique(char[] s) { return s; } // DMD 2.006
 
 
 //public import wx.Defs;
