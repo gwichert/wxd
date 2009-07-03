673bfe801b2d4d7932af1515ee33da7326d0a455
Anders F Bjorklund <afb@users.sourceforge.net>
2009-07-03 12:32:51 +0000

    OPTLINK doesn't support -l

--- wxc/dmd-wrap-libs.cpp
+++ wxc/dmd-wrap-libs.cpp
@@ -2,17 +2,21 @@
 // "dmd" uses weird LDFLAGS syntax, so we have to wrap params first
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
     char *param;
+    char *libdir;
     for (int i = 1; i < argc; i++)
     {
         if (i != 1)
             printf(" ");
         param = argv[i];
-        if (param[0] == '-' && param[1] == 'l')
-            printf("-L%s.lib", param+2);
+        if (param[0] == '-' && param[1] == 'L')
+            libdir = param+2;
+        else if (param[0] == '-' && param[1] == 'l')
+            printf("%s.lib", param+2);
         else
             printf("-L%s", param);
     }
