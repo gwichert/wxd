0f54d8e789747704a1889fcc4329b5a76faff3d9
Anders F Bjorklund <afb@users.sourceforge.net>
2010-12-28 21:58:52 +0000

    sort out multiple defines for platform

--- wxc/wx-platform.sh
+++ wxc/wx-platform.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 if [ "x$WX_CONFIG" = "x" ]; then WX_CONFIG=wx-config; fi
 # Outputs the current wxWidgets platform (from wx-config)
-$WX_CONFIG --cflags $@ | sed -e 's/.*\-D__\(WX.*\)__.*/\1/'
+$WX_CONFIG --cflags $@ | tr ' ' '\n' | grep __ | grep -v __WXDEBUG__ |
+sed -e 's/.*\-D__\(WX.*\)__.*/\1/' | head -n 1
